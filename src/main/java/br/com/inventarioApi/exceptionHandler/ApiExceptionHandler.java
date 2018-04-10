06c31bfd508c441edefefe0173893a5d38e8bd71
SamuelEngComp <samuellima810@gmail.com>
2018-04-10 13:34:19 -0300

    Finalizando entidade Pessoa

--- src/main/java/br/com/inventarioApi/exceptionHandler/ApiExceptionHandler.java
+++ src/main/java/br/com/inventarioApi/exceptionHandler/ApiExceptionHandler.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +16,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -33,7 +36,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler{
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
 		
 		String msgUsuario = "Mensagem Inválida";
-		String msgDesenvolvedor = ex.getCause().toString();
+		String msgDesenvolvedor = ex.getCause() != null ?ex.getCause().toString():ex.toString();
 		
 		List<Erro> erros = Arrays.asList(new Erro(msgDesenvolvedor, msgUsuario));
 
@@ -64,6 +67,19 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler{
 		}
 		
 		return erros;
+	}
+	
+	
+	@ExceptionHandler({EmptyResultDataAccessException.class})
+	public ResponseEntity<Object> EmptyResultDataAccessException(EmptyResultDataAccessException ex, WebRequest request) {
+		
+		String msgUsuario = "Recurso não encontrado";
+		String msgDesenvolvedor = ex.toString();
+		
+		List<Erro> erros = Arrays.asList(new Erro(msgDesenvolvedor, msgUsuario));
+		
+		return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+		
 	}
 	
 	
--- src/main/java/br/com/inventarioApi/service/PessoaService.java
+++ src/main/java/br/com/inventarioApi/service/PessoaService.java
@@ -2,7 +2,9 @@ package br.com.inventarioApi.service;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import br.com.inventarioApi.model.Pessoa;
@@ -16,10 +18,60 @@ public class PessoaService {
 	
 	
 	//listando
-		public List<Pessoa> listar(){
-			return pessoaRepository.findAll();
-		}
+	public List<Pessoa> listar(){
+		return pessoaRepository.findAll();
+	}
 	
+	
+	//atualizar pessoa	
+	public Pessoa atualizar(Integer codigo, Pessoa pessoa) {
+		
+		Pessoa pessoaSalva = buscarPessoaPeloCodigo(codigo);
+		
+		//copiando as propriedades de pessoa e ignorando o codigo
+		BeanUtils.copyProperties(pessoa, pessoaSalva, "id");
+		
+		//salvando a pessoa atualizada
+		return pessoaRepository.save(pessoaSalva);
+	}
+
+
+
+	//metodo criado para buscar pessoa pelo id
+	private Pessoa buscarPessoaPeloCodigo(Integer codigo) {
+		
+		//buscando a pessoa pelo codigo
+		Pessoa pessoaSalva = pessoaRepository.findOne(codigo);
+		
+		if(pessoaSalva == null) {
+			
+			//se a pessoa vinher nula
+			throw new EmptyResultDataAccessException(1);
+		}
+		return pessoaSalva;
+	}
+
+
+	
+	//atualizar propriedade ativo
+	public void atualizarPropriedadeAtivoPessoa(Integer codigo, Boolean ativo) {
+		Pessoa pessoaEncontrada = buscarPessoaPeloCodigo(codigo);
+		pessoaEncontrada.setAtivo(ativo);
+		
+		pessoaRepository.save(pessoaEncontrada);
+		
+	}
 	
 
 }
+
+
+
+
+
+
+
+
+
+
+
